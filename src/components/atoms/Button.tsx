43acacf104b529badc79da4f22159dfa0a310ab7
MMelchiorri <mm.melchiorri@gmail.com>
2025-02-17 08:30:34 +0100

    added prettier config

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,12 +1,11 @@
-import { ReactNode } from "react";
-import button from "@/styles/button.module.css"
+import { ReactNode } from 'react'
+import button from '@/styles/button.module.css'
 
 type ButtonFieldProps = {
-    children: ReactNode;
-    onClick: () => void
-};
+  children: ReactNode
+  onClick: () => void
+}
 
 export default function Button({ children }: ButtonFieldProps) {
-    return <button className={button.navBar}>{children}</button>;
-
+  return <button className={button.navBar}>{children}</button>
 }
--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -1,10 +1,10 @@
-import {ReactNode} from "react";
+import { ReactNode } from 'react'
 
 type InputProps = {
-    value: string,
-    handleChange: ()=> void 
+  value: string
+  handleChange: () => void
 }
 
-export default function Input ({value, handleChange}: InputProps):ReactNode {
-    return <input type="text" value={value} onChange={handleChange}/>
+export default function Input({ value, handleChange }: InputProps): ReactNode {
+  return <input type="text" value={value} onChange={handleChange} />
 }
