e6378635336e0bafde29880d59723e7de6724cb7
MMelchiorri <mm.melchiorri@gmail.com>
2025-02-21 23:20:43 +0100

    added prettier config

--- src/components/organisms/NavBar.tsx
+++ src/components/organisms/NavBar.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client'
 
-import NavLink from "../atoms/NavLink";
-import { NavBarProps } from "@/types/NavBarProps";
+import NavLink from '../atoms/NavLink'
+import { NavBarProps } from '@/types/NavBarProps'
 
 export default function Navbar({ items }: NavBarProps) {
-    return (
-        <nav>
-            <ul>
-                {items.map((elem, index) => (
-                    <li key={index}>
-                        <NavLink name={elem.name} link={elem.link} />
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul>
+        {items.map((elem, index) => (
+          <li key={index}>
+            <NavLink name={elem.name} link={elem.link} />
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
 }
