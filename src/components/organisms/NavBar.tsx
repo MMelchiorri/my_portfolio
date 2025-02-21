aefe625b52f1248a37cdf66553da61b07b992ef8
marco <mm.melchiorri@gmail.com>
2025-02-22 00:04:36 +0100

    add css nabvar

--- src/components/organisms/NavBar.tsx
+++ src/components/organisms/NavBar.tsx
@@ -2,17 +2,16 @@
 
 import NavLink from '../atoms/NavLink'
 import { NavBarProps } from '@/types/NavBarProps'
+import nav from '@/styles/nav.module.css'
 
 export default function Navbar({ items }: NavBarProps) {
   return (
-    <nav>
-      <ul>
+    <nav className={nav.navbar}>
+      <div className={nav.container}>
         {items.map((elem, index) => (
-          <li key={index}>
-            <NavLink name={elem.name} link={elem.link} />
-          </li>
+          <NavLink key={index} name={elem.name} link={elem.link} />
         ))}
-      </ul>
+      </div>
     </nav>
   )
 }
